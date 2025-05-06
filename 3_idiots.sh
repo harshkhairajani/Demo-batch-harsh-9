a67a5c0c38bfd0e6e855f8ff9d28b7a0af02bda7
Ubuntu <ubuntu@ip-172-31-40-7.eu-west-1.compute.internal>
2025-05-06 08:45:24 +0000

    We have added thev 3 iditos file in repo

---
+++ 3_idiots.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+hero="rancho"
+villain="virus"
+
+echo "3 iditos ka hero hai $hero"
+
+echo "3 iditos ka villain hai $villain"
+
+#shell/ environment variable
+echo "Current logged in user $USER"
+
+
+#user input
+read -p "Rancho ka poora naam kya tha" fullname
+
+echo "Rancho ka poora naam $fullname tha"
+
+
+# ./3iditos Rancho raju farhan
+
+echo "movie ka naam $0"
+
+echo "first iditos $1"
+
+echo "second iditos $2"
+
+echo "Third iditos $3"
+
+echo "Total member in movies are $#" 
+
+echo "ALL 3 iditos are $@"
+
+
